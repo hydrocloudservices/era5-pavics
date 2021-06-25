c93600edd5fe22142f712bb62af34275c27ee16a
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2021-06-25 15:45:56 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 class Config(object):
 
     # Bucket configuration
-    BUCKET = 's3://era5-atlantic-northeast/netcdf/single-levels/day'
-    CLIENT_KWARGS = {'endpoint_url': 'https://s3.us-east-2.wasabisys.com',
-                     'region_name': 'us-east-2'}
+    BUCKET = 's3://era5/world/reanalysis/single-levels/netcdf'
+    CLIENT_KWARGS = {'endpoint_url': 'https://s3.us-east-1.wasabisys.com',
+                     'region_name': 'us-east-1'}
     CONFIG_KWARGS = {'max_pool_connections': 30}
     PROFILE = 'default'
 
