e101edbac58e74cf5a42086566df680773eaef7e
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2021-07-13 13:12:11 -0400

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     # Dataset
     START_DATE = "1979-01-01"
-    END_DATE = (datetime.utcnow() - timedelta(days=5)).strftime('%Y-%m-%d')
+    END_DATE = (datetime.utcnow() - timedelta(days=6)).strftime('%Y-%m-%d')
 
     VARIABLES = {'2m_temperature': 't2m',
                  'total_precipitation': 'tp'} # 'snowfall': 'sf'
